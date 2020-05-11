a031333e3c694e7fe81875ee8d442fe52487b8c5
teklithuania <61139751+teklithuania@users.noreply.github.com>
2020-05-11 20:48:50 +0300

    Logic

---
+++ src/fibonacicalculator/FibonaciCalculator.java
@@ -0,0 +1,34 @@
+
+package fibonacicalculator;
+
+public class FibonaciCalculator {
+    public static int fc(int no1){
+       if (no1 <= 1){
+           return no1;
+       }
+       return fc(no1-1)+fc(no1-2);
+    }
+    public static int fc2(int a, int b, int n){
+        System.out.println("-----------------");
+        int c = 0;
+        System.out.println(c);
+        System.out.println(a);
+        System.out.println(b);
+        for (int i = 0; i < n; i++) {
+            if(a == 0 || b == 0 || n == 0){
+                break;
+            }
+            c = a + b;
+            a = b;
+            b = c;
+            System.out.println(c);
+        }
+        return 0;
+    }
+    public static void main(String[] args) {
+        for (int i = 0; i < 14; i++) {  
+        System.out.println(fc(i));
+        }
+        System.out.println(fc2(1, 1, 11));
+    }
+}
